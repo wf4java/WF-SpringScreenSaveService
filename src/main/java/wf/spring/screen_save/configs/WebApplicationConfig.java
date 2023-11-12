d6aeff891a292307d81df43f55c40f0ac443d16b
Umalt <momopopo659@gmail.com>
2023-11-12 19:27:21 +0300

    Commit

--- src/main/java/wf/spring/screen_save/configs/WebApplicationConfig.java
+++ src/main/java/wf/spring/screen_save/configs/WebApplicationConfig.java
@@ -7,14 +7,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import wf.spring.screen_save.properties.ConfigProperties;
 import wf.spring.screen_save.properties.ImageStorageProperties;
 import wf.spring.screen_save.properties.JwtProperties;
 import wf.spring.screen_save.properties.VerificationProperties;
 
 
 @Configuration
-@EnableConfigurationProperties({JwtProperties.class, ConfigProperties.class, ImageStorageProperties.class, VerificationProperties.class})
+@EnableConfigurationProperties({JwtProperties.class, ImageStorageProperties.class, VerificationProperties.class})
 @EnableScheduling
 @RequiredArgsConstructor
 public class WebApplicationConfig {
